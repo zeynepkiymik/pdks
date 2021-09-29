e2c60c5e6a35e4be79088f691f798c098c9868ed
zeynepkiymik <33024264+zeynepkiymik@users.noreply.github.com>
2021-09-29 14:57:52 +0300

    Update GirisController.php

--- app/Http/Controllers/GirisController.php
+++ app/Http/Controllers/GirisController.php
@@ -4,11 +4,12 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\pdksz;
+
 class GirisController extends Controller
 {
-  public function blog(Request $request){
-   $data['pdksz'] = pdksz::all();
-     return view('pdksz', $data);
+  public function girisler (Request $request){
+   $data['pdksz'] = pdksz::all()->where('type' , 'giris');
+     return view('girisler', $data);
 //verileri datadan çek ekrana yaz
  }
 }
