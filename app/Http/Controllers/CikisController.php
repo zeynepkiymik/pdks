1acab6545175cb75f30bd67c202e0930fc6309c3
zeynepkiymik <33024264+zeynepkiymik@users.noreply.github.com>
2021-09-29 14:57:58 +0300

    Update CikisController.php

--- app/Http/Controllers/CikisController.php
+++ app/Http/Controllers/CikisController.php
@@ -3,9 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\pdksz;
 
 class CikisController extends Controller
 {
-  public function cikis (){
-   return view('cikis');
+  public function cikislar(Request $request){
+   $data['pdksz'] = pdksz::all()->where('type' , 'cikis');
+     return view('cikislar', $data);
+//verileri datadan çek ekrana yaz
+ }
+
 }
